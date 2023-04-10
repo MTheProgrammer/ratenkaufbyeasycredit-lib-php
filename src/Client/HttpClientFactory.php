eb3cc21307220a073a772e5e532487acebf26849
Matt Chad <maciejcha@gmail.com>
2023-04-10 14:13:15 +0800

    Update php version constraints to 8.0-8.2

--- src/Client/HttpClientFactory.php
+++ src/Client/HttpClientFactory.php
@@ -1,8 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Netzkollektiv\EasyCreditApi\Client;
 
-class HttpClientFactory {
-	public function getClient($url, $params) {
-		return new \Laminas\Http\Client($url, $params);
-	}
+use Laminas\Http\Client;
+
+class HttpClientFactory
+{
+    /**
+     * @param string $url
+     * @param array|null $params
+     * @return Client
+     */
+    public function getClient(string $url, ?array $params = null): Client
+    {
+        return new Client($url, $params);
+    }
 }
